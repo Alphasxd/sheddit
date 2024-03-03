89447a8ad7a67ffad2324c53a6452052efaf50bf
Sheldon <alphasxd@outlook.com>
2024-03-03 20:57:26 +0800

    Fix error handling and rate limiting middleware

--- middleware/limit.go
+++ middleware/limit.go
@@ -15,8 +15,8 @@ const OnceConsume = 1
 func RateLimit(fillInterval time.Duration, cap int64) func(c *gin.Context) {
 	bucket := ratelimit.NewBucket(fillInterval, cap)
 	return func(ctx *gin.Context) {
-		//if bucket.Take(OnceConsume) > 0 // take 返回的是还需要多少时间取到令牌
-		//bucket.TakeAvailable(OnceConsume) 返回可用的令牌数
+		// if bucket.Take(OnceConsume) > 0 // take 返回的是还需要多少时间取到令牌
+		// bucket.TakeAvailable(OnceConsume) 返回可用的令牌数
 		// 没有足够的令牌 可以选择Sleep等待或者直接返回
 		if bucket.TakeAvailable(OnceConsume) < OnceConsume {
 			ctx.JSON(http.StatusOK, gin.H{
