4f67077fce7073575a0684c0422ea8a7d3b72ce1
Sheldon <alphasxd@outlook.com>
2024-03-03 21:31:06 +0800

    Refactor snowflake package initialization and ID generation

--- pkg/snowflake/snowflake.go
+++ pkg/snowflake/snowflake.go
@@ -10,7 +10,7 @@ import (
 // 定义一个节点： 通过这个全局的 node，就可以用于制造 ID了。
 var node *snowflake.Node
 
-// 源码里有很多默认的基本设定，比如开始时间等等， 我们可以改变这些，自己初始化一个 node节点
+// Init 源码里有很多默认的基本设定，比如开始时间等等， 我们可以改变这些，自己初始化一个 node节点
 func Init(startTime string, machineID int64) {
 	var st time.Time
 	st, err := time.Parse("2006-01-02", startTime)
@@ -26,12 +26,7 @@ func Init(startTime string, machineID int64) {
 	zap.L().Info("snowflake node init success")
 }
 
-// 返回int64位的 id值
+// GenerateID 返回int64位的 id值
 func GenerateID() int64 {
 	return node.Generate().Int64()
-}
-
-func main() {
-	Init("2023-01-01", 1)
-	println(GenerateID())
 }
