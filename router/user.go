bae00e9414565b6cd2fc43bda42c761ff39b815a
Sheldon <alphasxd@outlook.com>
2024-03-03 20:56:02 +0800

    Fix formatting in user routes

--- router/user.go
+++ router/user.go
@@ -10,7 +10,7 @@ import (
 func GetUserRoutes(router *gin.RouterGroup) {
 	userGroup := router.Group("/user")
 	{
-		userGroup.GET("/info", middleware.AuthRequired(), api.GetUserInfoByID) //获取其他用户信息
+		userGroup.GET("/info", middleware.AuthRequired(), api.GetUserInfoByID) // 获取其他用户信息
 		userGroup.POST("/register", api.Register)                              // 用户注册
 		userGroup.POST("/login", api.Login)                                    // 用户登录
 	}
