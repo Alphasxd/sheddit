07e32a72f1bad5cdc5fc56b437e79ebebb176229
Sheldon <alphasxd@outlook.com>
2024-03-03 20:47:13 +0800

    Refactor MySQL configuration and database initialization

--- config/mysql.go
+++ config/mysql.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"time"
+
 	"sheddit/pkg/snowflake"
 
 	"go.uber.org/zap"
@@ -67,7 +68,6 @@ func InitDB(conf *MySQLConfig) *gorm.DB {
 			SingularTable: true, // 使用单数
 		},
 	})
-
 	if err != nil {
 		zap.L().Error("failed to connect database ,err:" + err.Error())
 		panic(err)
@@ -79,37 +79,31 @@ func InitDB(conf *MySQLConfig) *gorm.DB {
 		panic(err)
 	}
 	sqlDB.SetMaxIdleConns(10)           // 设置连接池中空闲连接的最大数量
-	sqlDB.SetMaxOpenConns(100)          //设置打开数据库连接的最大数量
-	sqlDB.SetConnMaxLifetime(time.Hour) //设置了连接可复用的最大时间
-	//数据库迁移
+	sqlDB.SetMaxOpenConns(100)          // 设置打开数据库连接的最大数量
+	sqlDB.SetConnMaxLifetime(time.Hour) // 设置了连接可复用的最大时间
+	// 数据库迁移
 	db.AutoMigrate(&model.User{})
 	db.AutoMigrate(&model.Category{})
-	db.Create(&model.User{BaseModel: model.BaseModel{ID: 1},
-		UserID:   snowflake.GenerateID(),
-		Username: "admin", Password: "admin",
+	db.Create(&model.User{
+		BaseModel: model.BaseModel{ID: 1},
+		UserID:    snowflake.GenerateID(),
+		Username:  "admin", Password: "admin",
 		Gender: "male",
-		Age:    11})
-	db.Create(&model.User{BaseModel: model.BaseModel{ID: 2},
-		UserID:   snowflake.GenerateID(),
-		Username: "novo", Password: "novo",
+		Age:    11,
+	})
+	db.Create(&model.User{
+		BaseModel: model.BaseModel{ID: 2},
+		UserID:    snowflake.GenerateID(),
+		Username:  "novo", Password: "novo",
 		Gender: "male",
-		Age:    11})
+		Age:    11,
+	})
 	db.Create(&model.Category{BaseModel: model.BaseModel{ID: 1}, Name: "Go", Description: "Go社区"})
 	db.Create(&model.Category{BaseModel: model.BaseModel{ID: 2}, Name: "Java", Description: "Java社区"})
 	db.Create(&model.Category{BaseModel: model.BaseModel{ID: 3}, Name: "LeetCode", Description: "算法社区"})
 	db.Create(&model.Category{BaseModel: model.BaseModel{ID: 4}, Name: "Acwing", Description: "算法社区"})
 
 	db.AutoMigrate(&model.Post{})
-	// db.AutoMigrate(&model.Resource{})
-	// db.AutoMigrate(&model.Liked{})       //点赞表
-	//db.AutoMigrate(&model.Collect{})     //收藏表
-	//db.AutoMigrate(&model.Collection{})  //收藏夹表
-	//db.AutoMigrate(&model.Follow{})      //关注表
-	//db.AutoMigrate(&model.Comment{})     //评论表
-	//db.AutoMigrate(&model.Announce{})    //公告表
-	//db.AutoMigrate(&model.UserMessage{}) //私信表
-	//db.AutoMigrate(&model.Danmaku{})     //弹幕表
-	//db.AutoMigrate(&model.Carousel{})    //轮播图表
 	zap.L().Info("database init success")
 	DB = db
 	return db
