5609cb1b4ac7aa03247a80e69ceb3af3854d75a1
Sheldon <alphasxd@outlook.com>
2024-03-03 20:47:50 +0800

    Remove commented out code in redis.go

--- config/redis.go
+++ config/redis.go
@@ -28,27 +28,4 @@ func InitRedis(cfg *RedisConfig) {
 		panic(err)
 	}
 	zap.L().Info("redis init success")
-	return
-	//
-	//err := rdb.Set(ctx, "key", "value", 0).Err()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//val, err := rdb.Get(ctx, "key").Result()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("key", val)
-	//
-	//val2, err := rdb.Get(ctx, "key2").Result()
-	//if err == redis.Nil {
-	//	fmt.Println("key2 does not exist")
-	//} else if err != nil {
-	//	panic(err)
-	//} else {
-	//	fmt.Println("key2", val2)
-	//}
-	// Output: key value
-	// key2 does not exist
 }
